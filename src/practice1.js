4c5d33f12d8e3d5532ec7661cc15ee1023c60f96
Yuyue Gao <gaoyuyue@outlook.com>
2020-07-04 09:30:31 +0800

    es6 syntax homework

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,11 @@
-const parseData
+const parseData = ({data, column}) => {
+    return data.map(item => {
+        let obj = {};
+        for (const [i, {name}] of column.entries()) {
+            obj[name] = item[i];
+        }
+        return obj;
+    });
+};
+
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,8 @@
-const inject
-export { inject };
+const inject = (items, sections) => {
+    const result = sections.reduce((acc, {content, index}) =>
+        [...acc.slice(0, index), [content, acc[index]], ...acc.slice(index + 1)], items);
+
+    // eq result.flat()
+    return [].concat(...result);
+};
+export {inject};
